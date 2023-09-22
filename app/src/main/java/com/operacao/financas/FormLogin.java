c9826a42b7b16d376c9b309815e25e5f36322437
Anderson <anderson21fonseca@gamil.com>
2023-09-22 18:42:56 -0300

    Criado a tela de login

--- app/src/main/java/com/operacao/financas/FormLogin.java
+++ app/src/main/java/com/operacao/financas/FormLogin.java
@@ -1,38 +1,38 @@
 package com.operacao.financas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class FormLogin extends AppCompatActivity {
     private  DaoDados dados;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        dados = new DaoDados(this);
-       // addUsuario();
-         setaUsuario();
+        setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+        // addUsuario();
+        //setaUsuario();
     }
 
+
     public void setaUsuario(){
-      boolean log=  dados.setDadosUsuario("AndersonDev","1234");
-     // boolean log=true;
-      String msg="";
-      if(log){
-          msg="Tem esse usuário cadastrado";
-      }else {
-          msg="Não tem esses dados no banco de dados";
-      }
+        boolean log=  dados.setDadosUsuario("AndersonDev","1234");
+        // boolean log=true;
+        String msg="";
+        if(log){
+            msg="Tem esse usuário cadastrado";
+        }else {
+            msg="Não tem esses dados no banco de dados";
+        }
         Toast.makeText(this, " "+msg, Toast.LENGTH_SHORT).show();
     }
 
     public void  addUsuario(){
         ModeloUsuario mdUsuario = new ModeloUsuario(0,"Anderson","8499214567","AndersonDev","1234");
-     long dd= dados.inserirUsuario(mdUsuario);
+        long dd= dados.inserirUsuario(mdUsuario);
         Toast.makeText(this, "Dados do usuário adicionado com sucesso! "+dd, Toast.LENGTH_SHORT).show();
     }
 }
